bb3bdb6990e0b2f51f89adffe6701cdd541f7975
Alex Norton <fidgetting@gmail.com>
2013-11-26 11:25:08 -0700

    Update Tracer.cpp
    Changed the Tracer binary to not use the boost program options library. How it was working didn't really make sense and this makes it much simpler.

--- src/Tracer.cpp
+++ src/Tracer.cpp
@@ -15,14 +15,14 @@
 #include <iostream>
 
 /* boost includes */
-#include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
-namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
 /* other */
 #include <gtkmm.h>
 #include <gdk/gdk.h>
+
+const char* usage = "Usage: Tracer <model file> <output file>";
 
 Glib::RefPtr<Gdk::Pixbuf> copyOut(const ray::Matrix<ray::Pixel> img) {
   Glib::RefPtr<Gdk::Pixbuf> ret = Gdk::Pixbuf::create(
@@ -46,24 +46,26 @@ int main(int argc, char** argv) {
   Glib::RefPtr<Gtk::Application> app =
       Gtk::Application::create(argc, argv);
 
-  /* check program options */
-  po::options_description opt("Options");
-  opt.add_options()
-      ("help,h",                           "produce help message")
-      ("model",  po::value<std::string>(), "model input file")
-      ("output", po::value<std::string>(), "image output file");
-
-  po::variables_map vm;
-  po::store(po::command_line_parser(argc, argv).options(opt).run(), vm);
-  po::notify(vm);
-
-  if(vm.count("help") || !vm.count("model") || !vm.count("output")) {
-    opt.print(std::cout);
+  if(argc != 3) {
+    std::cout << usage << std::endl;
     return -1;
+  }
+  
+  /* validate inputs */
+  fs::path model  = argv[1];
+  fs::path result = argv[2];
+  
+  if(!fs::is_regular_file(model)) {
+    std::cout << usage << std::endl;
+    return -1;
+  }
+  
+  if(fs::is_directory(result)) {
+    result = result / "out.png";
   }
 
   /* load the model */
-  auto stream = ray::ObjectStream::loadObject(vm["model"].as<std::string>());
+  auto stream = ray::ObjectStream::loadObject(model);
 
   ray::Model  model;
   ray::Camera camera;
@@ -71,11 +73,9 @@ int main(int argc, char** argv) {
   ray::Model::fromObjectStream(stream, model, camera);
 
   /* render the image */
-  fs::path output(vm["output"].as<std::string>());
-
   try {
     copyOut(model.click(camera, 1024, 1024))->save(
-      output.string(), output.extension().string().substr(1));
+      result.string(), result.extension().string().substr(1));
   } catch(Gdk::PixbufError& error) {
     std::cout << error.what() << std::endl;
   }
